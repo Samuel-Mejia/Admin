b322bfb2f88a683b8814ab7688679bf984d689fc
Samuel Mejía <sf977996@gmail.com>
2024-05-12 12:35:31 -0600

    Busero

--- DSS-desafio3/resources/views/perfil.blade.php
+++ DSS-desafio3/resources/views/perfil.blade.php
@@ -1,18 +1,111 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div class="container">
-    <h1>Perfil del Usuario</h1>
-    <p>Nombre: {{ $empleado->name }} {{ $empleado->lastname }}</p>
-    <p>Email: {{ $empleado->email }}</p>
-    <p>Cargo: {{ $empleado->cargo }}</p>
-    <p>Salario: {{ $empleado->salario }}</p>
-</div>
-</body>
-</html>
 
+
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+    <style>
+        .bi-person {
+            color: black;
+        }
+    h5{
+        color: black;
+    }
+    .gradient-custom {
+/* fallback for old browsers */
+background: #f6d365;
+
+/* Chrome 10-25, Safari 5.1-6 */
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to right bottom, rgba(131, 177, 224, 0.85), rgba(178, 219, 191, 0.85));
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to right bottom, rgba(131, 177, 224, 0.85), rgba(178, 219, 191, 0.85));
+    }
+</style>
+    <body>
+    
+
+
+<section class="vh-100" style="background-color: #f4f5f7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-6 mb-4 mb-lg-0">
+        <div class="card mb-3" style="border-radius: .5rem;">
+          <div class="row g-0">
+            <div class="col-md-4 gradient-custom text-center text-white"
+              style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+              <br>
+              <svg xmlns="http://www.w3.org/2000/svg" width="106" height="106" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+</svg>
+             <br>
+             <br>
+              <h5>{{ $empleado->name }} {{ $empleado->lastname }}</h5>
+              
+              <br>
+            
+            </div>
+            <div class="col-md-8">
+              <div class="card-body p-4">
+              <center>
+                 <h6>Informacion del empleado</h6>
+              </center>  
+             
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Correo Electronico</h6>
+                    
+                    <p class="text-muted">- {{ $empleado->email }}</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Cargo</h6>
+                    <p>- {{ $empleado->cargo }}</p>
+                  </div>
+                </div>
+                <a href="{{ route('logout', $empleado->id)}}" type="button" class="btn btn-danger m-0">Salir</a>
+               
+      </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    
+
+
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
